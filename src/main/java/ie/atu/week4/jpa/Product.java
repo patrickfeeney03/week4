3f6e14f2494e6d1ac80472a972ff21cb6ddda227
Paul <paul.lennon@atu.ie>
2024-10-05 09:27:16 +0100

    Added validation

--- src/main/java/ie/atu/week4/jpa/Product.java
+++ src/main/java/ie/atu/week4/jpa/Product.java
@@ -1,5 +1,8 @@
 package ie.atu.week4.jpa;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +12,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Product
 {
+    @NotBlank(message = "Product name is mandatory")
     private String productName;
+    @NotBlank(message = "Product description is mandatory")
     private String productDescription;
+    @Min(value = 0, message = "Product price must be non-negative")
     private int productPrice;
+    @NotNull(message = "Product code is mandatory")
     private int productCode;
 }
