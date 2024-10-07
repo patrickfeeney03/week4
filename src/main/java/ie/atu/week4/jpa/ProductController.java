d9b24c96493b39604f0a7acdf6a477a6e9a9d83d
Patrick <g00409592@atu.ie>
2024-10-07 17:18:41 +0100

    v2

--- src/main/java/ie/atu/week4/jpa/ProductController.java
+++ src/main/java/ie/atu/week4/jpa/ProductController.java
@@ -3,14 +3,11 @@ package ie.atu.week4.jpa;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-
-    private List<Product> productList = new ArrayList<>();
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -18,48 +15,22 @@ public class ProductController {
     }
 
     @GetMapping("/getProducts")
-    public List<Product> getProducts() {
-        return productList;
+    public ResponseEntity<List<Product>> getProducts() {
+        return ResponseEntity.ok(productService.getAll());
     }
 
     @PostMapping("/addProduct")
     public ResponseEntity<List<Product>> addProduct(@RequestBody Product product) {
-        productList = productService.addProduct(product);
-        return ResponseEntity.ok(productList);
-    }
-
-    private Product findProductById(int id) {
-        for (Product product : productList) {
-            if (product.getProductCode() == id) {
-                return product;
-            }
-        }
-        return null;
+        return ResponseEntity.ok(productService.add(product));
     }
 
     @PutMapping("/updateProduct/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            existingProduct.setProductName(updatedProduct.getProductName());
-            existingProduct.setProductDescription(updatedProduct.getProductDescription());
-            existingProduct.setProductPrice(updatedProduct.getProductPrice());
-            return ResponseEntity.ok(existingProduct);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(productService.update(id, product));
     }
 
     @DeleteMapping("/deleteProduct/{id}")
-    public ResponseEntity<List<Product>> deleteProduct(@PathVariable int id) {
-        Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            productList.remove(existingProduct);
-            return ResponseEntity.ok(productList);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.delete(id));
     }
 }
--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -1,6 +1,8 @@
 package ie.atu.week4.jpa;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,9 +15,45 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> addProduct(Product product) {
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    public List<Product> add(Product product) {
         productRepository.save(product);
 
         return productRepository.findAll();
+    }
+
+    public Product delete(Long id) {
+        Product existingProduct = findProductById(id);
+
+        if (existingProduct != null) {
+            productRepository.deleteById(existingProduct.getId());
+            return existingProduct;
+        }
+        throwNotFound();
+        return null;
+    }
+
+    public Product update(Long id, Product product) {
+        Product existingProduct = findProductById(id);
+
+        if (existingProduct != null) {
+            existingProduct.setProductName(product.getProductName());
+            existingProduct.setProductDescription(product.getProductDescription());
+            existingProduct.setProductPrice(product.getProductPrice());
+            return productRepository.save(existingProduct);
+        }
+        throwNotFound();
+        return null;
+    }
+
+    private void throwNotFound() throws ResponseStatusException {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find product. Not doing anything");
+    }
+
+    private Product findProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
     }
 }
