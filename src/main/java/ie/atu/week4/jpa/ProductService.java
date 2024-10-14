d8bd8f72a1c0c65b956282f9ec7482ab7649518e
Patrick <g00409592@atu.ie>
2024-10-14 12:23:37 +0100

    Make findProductById throw

--- src/main/java/ie/atu/week4/jpa/ProductService.java
+++ src/main/java/ie/atu/week4/jpa/ProductService.java
@@ -27,33 +27,21 @@ public class ProductService {
 
     public Product delete(Long id) {
         Product existingProduct = findProductById(id);
-
-        if (existingProduct != null) {
-            productRepository.deleteById(existingProduct.getId());
-            return existingProduct;
-        }
-        throwNotFound();
-        return null;
+        productRepository.deleteById(existingProduct.getId());
+        return existingProduct;
     }
 
     public Product update(Long id, Product product) {
         Product existingProduct = findProductById(id);
 
-        if (existingProduct != null) {
-            existingProduct.setProductName(product.getProductName());
-            existingProduct.setProductDescription(product.getProductDescription());
-            existingProduct.setProductPrice(product.getProductPrice());
-            return productRepository.save(existingProduct);
-        }
-        throwNotFound();
-        return null;
-    }
-
-    private void throwNotFound() throws ResponseStatusException {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find product. Not doing anything");
+        existingProduct.setProductName(product.getProductName());
+        existingProduct.setProductDescription(product.getProductDescription());
+        existingProduct.setProductPrice(product.getProductPrice());
+        return productRepository.save(existingProduct);
     }
 
     private Product findProductById(Long id) {
-        return productRepository.findById(id).orElse(null);
+        return productRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Could not find product."));
     }
 }
